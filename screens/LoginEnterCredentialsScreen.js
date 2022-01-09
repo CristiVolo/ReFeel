160529ec5dbe4615139f12c2652aee58a212ce5a
cristiherisanu <62358568+cristiherisanu@users.noreply.github.com>
2022-01-09 12:59:42 +0200

    forgotPassword and register for specialist functional
    without firestore fields (just auth)

--- screens/LoginEnterCredentialsScreen.js
+++ screens/LoginEnterCredentialsScreen.js
@@ -1,17 +1,52 @@
 import { useNavigation } from '@react-navigation/native'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { KeyboardAvoidingView,StyleSheet, Text,TextInput, View, TouchableOpacity } from 'react-native'
 import PurpleButton from '../components/PurpleButton'
 import TransparentButton from '../components/TransparentButton'
 import CustomTextInput from '../components/TextInput'
 import SecureTextInput from '../components/SecureTextInput'
+import { auth } from '../config/firebase'
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 
 
 const LoginEnterCredentialsScreen = () => {
     const navigation = useNavigation()
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+        if (user) {
+          navigation.replace("home");
+        } 
+      })
+  
+      return unsubscribe
+    }, [])
+
     const handleLogInByEnteringCredentials = () => {
-      navigation.replace("Login");
+
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+
     }
+
+    const handleForgotPassword = () => {
+      navigation.replace("ForgotPassword");
+    }
+
+    const defaultFunction=()=> {
+    }
+
     return (
     <KeyboardAvoidingView
       style={styles.container}
@@ -24,16 +59,16 @@ const LoginEnterCredentialsScreen = () => {
     >
         <Text style={styles.textStyle}>Enter User Credentials</Text>
         <View style={styles.buttonContainer}>
-        <CustomTextInput text={"E-MAIL ADDRESS"}>
+        <CustomTextInput text={"E-MAIL ADDRESS"} setText={setEmail}>
         </CustomTextInput>
-        <SecureTextInput text={"PASSWORD"}>
+        <SecureTextInput text={"PASSWORD"} setPassword={setPassword}>
         </SecureTextInput>
         <PurpleButton text={"AUTHENTICATE"} onPress={handleLogInByEnteringCredentials}>
         </PurpleButton>
         
        </View>
       <View style={styles.button}>
-        <TransparentButton text={"FORGOT PASSWORD?"}>
+        <TransparentButton text={"FORGOT PASSWORD?"} onPress={handleForgotPassword}>
         </TransparentButton>
       </View>
     </KeyboardAvoidingView>
