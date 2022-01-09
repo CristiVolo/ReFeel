a1b9e3ce7ed6abc8798bc5e394efaea398c74f57
CristiVolo <cristianvolosciuc@yahoo.com>
2022-01-09 21:24:56 +0200

    Push required to avoid merge conflict

--- App.js
+++ App.js
@@ -9,7 +9,10 @@ import LoginSendEmailScreen from './screens/LoginSendEmailScreen';
 import LoginSendSMSScreen from './screens/LoginSendSMSScreen';
 import LoginEnterCodeScreen from './screens/LoginEnterCodeScreen';
 import SignUpScreen from './screens/SignUpScreen';
-import MapScreen from './screens/MapScreen';
+import SpecialistDataScreen from './screens/SpecialistDataScreen'
+import home from './screens/home'
+import ForgotPasswordScreen from './screens/ForgotPasswordScreen'
+
 
 
 const Stack = createNativeStackNavigator(
@@ -20,13 +23,15 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{headerShown: false}} name="Map" component={MapScreen} />
         <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
         <Stack.Screen options={{headerShown: false}} name="LoginEnterCredentials" component={LoginEnterCredentialsScreen} />
         <Stack.Screen options={{headerShown: false}} name="LoginSendEmail" component={LoginSendEmailScreen} />
         <Stack.Screen options={{headerShown: false}} name="LoginSendSMS" component={LoginSendSMSScreen} />
         <Stack.Screen options={{headerShown: false}} name="LoginEnterCode" component={LoginEnterCodeScreen} />
         <Stack.Screen options={{headerShown: false}} name="SignUp" component={SignUpScreen} />
+        <Stack.Screen options={{headerShown: false}} name="SpecialistData" component={SpecialistDataScreen} />
+        <Stack.Screen options={{headerShown: false}} name="home" component={home} />
+        <Stack.Screen options={{headerShown: false}} name="ForgotPassword" component={ForgotPasswordScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
